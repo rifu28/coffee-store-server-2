dc1591605ed84876778c7451fcb14cc2ae97636c
rifu28 <rifatmeumeu28@gmail.com>
2024-09-20 23:50:49 +0600

    after two days

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -36,6 +36,13 @@ async function run() {
     app.post("/coffee", async (req, res) => {
       const newCoffee = req.body;
       const result = await coffeCollections.insertOne(newCoffee);
+      res.send(result);
+    });
+
+    app.delete("/coffee/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await coffeCollections.deleteOne(query);
       res.send(result);
     });
 
